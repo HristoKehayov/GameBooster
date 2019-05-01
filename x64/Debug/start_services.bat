a68bc20af692f772f459f882a9b093e6d42181d3
HristoKehayov <hristokehayov@gmail.com>
2019-05-02 01:38:08 +0300

    fixed some bugs

--- x64/Debug/start_services.bat
+++ x64/Debug/start_services.bat
@@ -1,26 +1,28 @@
-net stop Spooler
-net stop Xbox Live Auth Manager
-net start Xbox Live Game Save
+net start Spooler
+net start XblAuthManager
+net start XblGameSave
 net start XboxNetApiSvc
-net start Bluetooth Support Service
-net start Application Layer Gateway Service
-net start AllJoyn Router Service
-net start Windows Insider Service
-net start Remote Desktop Configuration and Remote Desktop Services
-net start Downloaded Maps Manager
-net start Windows Mobile Hotspot Service
-net start Downloaded Maps Manager
+net start bthserv
+net start ALG
+net start wisvc
+net start SessionEnv
+net start TermService
+net start UmRdpService
+net start RasMan
+net start RasAuto
+net start MapsBroker
+net start icssvc
+net start MapsBroker
 net start Fax
-net start Geolocation Service
-net start Hyper-V Data Exchange Service
-net start Hyper-V Guest Service Interface
-net start Hyper-V Guest Shutdown Service
-net start Hyper-V Heartbeat Service
-net start Hyper-V Remote Desktop Virtualization Service
-net start Hyper-V Hyper-V Time Synchronization Service
-net start Hyper-V VM Session Service
-net start Hyper-V Volume Shadow Copy Requestor
-net start Microsoft Diagnostics Hub Standard Collector Service
-net start Remote Procedure Call (RPC) Locator
+net start lfsvc
+net start vmickvpexchange
+net start vmicguestinterface
+net start vmicshutdown
+net start start vmicheartbeat
+net start vmicvmsession
+net start vmicrdv
+net start vmictimesync
+net start vmicvss
+net start diagnosticshub.standardcollector.service
 
 
--- x64/Debug/stop_services.bat
+++ x64/Debug/stop_services.bat
@@ -1,26 +1,28 @@
 net stop Spooler
-net stop Xbox Live Auth Manager
-net stop Xbox Live Game Save
+net stop XblAuthManager
+net stop XblGameSave
 net stop XboxNetApiSvc
-net stop Bluetooth Support Service
-net stop Application Layer Gateway Service
-net stop AllJoyn Router Service
-net stop Windows Insider Service
-net stop Remote Desktop Configuration and Remote Desktop Services
-net stop Downloaded Maps Manager
-net stop Windows Mobile Hotspot Service
-net stop Downloaded Maps Manager
+net stop bthserv
+net stop ALG
+net stop wisvc
+net stop SessionEnv
+net stop TermService
+net stop UmRdpService
+net stop RasMan
+net stop RasAuto
+net stop MapsBroker
+net stop icssvc
+net stop MapsBroker
 net stop Fax
-net stop Geolocation Service
-net stop Hyper-V Data Exchange Service
-net stop Hyper-V Guest Service Interface
-net stop Hyper-V Guest Shutdown Service
-net stop Hyper-V Heartbeat Service
-net stop Hyper-V Remote Desktop Virtualization Service
-net stop Hyper-V Hyper-V Time Synchronization Service
-net stop Hyper-V VM Session Service
-net stop Hyper-V Volume Shadow Copy Requestor
-net stop Microsoft Diagnostics Hub Standard Collector Service
-net stop Remote Procedure Call (RPC) Locator
+net stop lfsvc
+net stop vmickvpexchange
+net stop vmicguestinterface
+net stop vmicshutdown
+net stop vmicheartbeat
+net stop vmicvmsession
+net stop vmicrdv
+net stop vmictimesync
+net stop vmicvss
+net stop diagnosticshub.standardcollector.service
 
 
